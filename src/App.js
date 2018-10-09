cccae94a98590b19752f06ec7edc710ae9e2705e
BolgarMaxym97 <bolgar.m.wezom@gmail.com>
2018-10-09 22:26:22 +0300

    material ui added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Grid} from '@material-ui/core';
+import {RaisedButton} from 'material-ui';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <Grid container>
+                    <Grid item xs={3}>
+                        <RaisedButton label="Default" primary={true}/>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <RaisedButton label="Default" primary={true}/>
+                    </Grid>
+                </Grid>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
