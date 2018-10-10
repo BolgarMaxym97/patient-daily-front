b64f3adbf8c7400dc214f888cbb9ed72ebae3ad4
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-10-10 17:23:47 +0300

    Layout added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Layout from './Layout/Layout';
 import Login from './Auth/Login/Login';
-import {AppBar} from 'material-ui';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <MuiThemeProvider>
-                <AppBar
-                    title="Login"
-                />
-                <Login/>
+                <Layout title="Title">
+                    <Login/>
+                </Layout>
             </MuiThemeProvider>
         );
     }
