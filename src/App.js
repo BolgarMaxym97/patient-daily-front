a0fa113c9c0e7a3e1bd0c15fda273f767de58eaf
@BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-10-13 17:45:53 +0300

    app architecture changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Layout from './Layout/Layout';
-import Login from './Auth/Login/Login';
+import Layout from './containers/Layout/Layout';
+import Login from './containers/Auth/Login/Login';
 import './App.css';
 
 class App extends Component {
