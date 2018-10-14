012ea989e958ca109aa890a678c522852b04e497
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-10-14 17:28:40 +0300

    auth card component added

--- src/containers/Auth/Login/Login.js
+++ src/containers/Auth/Login/Login.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Grid, Typography, Paper} from '@material-ui/core';
 import {RaisedButton, TextField, RadioButton, RadioButtonGroup} from 'material-ui';
+import AuthCard from '../AuthCard';
+import classes from '../AuthCard.module.css';
 
 class Login extends Component {
 
@@ -23,75 +25,45 @@ class Login extends Component {
     }
 
     render() {
-        const style = {
-            centerFields: {
-                margin: '10px auto 0 auto',
-                textAlign: 'center'
-            },
-            centerRadioButtons: {
-                margin: '0 auto 10px auto',
-                width: '8%'
-            },
-            bottomButtons: {
-                margin: '10px'
-            },
-            paper: {
-                margin: '5% auto',
-                padding: '2%'
-            },
-            loginDesc: {
-                fontSize: '12px'
-            }
-        };
         return (
-            <Grid container>
-                <Paper elevation={20} style={style.paper}>
-                    <Grid item xs={12} style={style.centerFields}>
-                        <Typography component="h2" variant="h5" gutterBottom>
-                            <b>Patient Daily</b>
-                        </Typography>
-                        <p style={style.loginDesc}>(enter your email and password)</p>
-                    </Grid>
-                    <Grid item xs={12} style={style.centerFields}>
-                        <TextField
-                            hintText="Enter your Username"
-                            floatingLabelText="Username"
-                            onChange={(event, newValue) => this.setState({username: newValue})}
+            <AuthCard>
+                <Grid item xs={12} className={classes.centerFields}>
+                    <TextField
+                        hintText="Enter your Username"
+                        floatingLabelText="Username"
+                        onChange={(event, newValue) => this.setState({username: newValue})}
+                    />
+                </Grid>
+                <Grid item xs={12} className={classes.centerFields}>
+                    <TextField
+                        type="password"
+                        hintText="Enter your Password"
+                        floatingLabelText="Password"
+                        onChange={(event, newValue) => this.setState({password: newValue})}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <RadioButtonGroup name="isHospital" defaultSelected="0"
+                                      onChange={(event, newValue) => this.setState({isHospital: newValue})}>
+                        <RadioButton
+                            value="0"
+                            label="Patient"
+                            className={classes.centerRadioButtons}
                         />
-                    </Grid>
-                    <Grid item xs={12} style={style.centerFields}>
-                        <TextField
-                            type="password"
-                            hintText="Enter your Password"
-                            floatingLabelText="Password"
-                            onChange={(event, newValue) => this.setState({password: newValue})}
+                        <RadioButton
+                            value="1"
+                            label="Hospital"
+                            className={classes.centerRadioButtons}
                         />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <RadioButtonGroup name="isHospital" defaultSelected="0"
-                                          onChange={(event, newValue) => this.setState({isHospital: newValue})}>
-                            <RadioButton
-                                value="0"
-                                label="Patient"
-                                className="radioGroup-item"
-                                style={style.centerRadioButtons}
-                            />
-                            <RadioButton
-                                value="1"
-                                label="Hospital"
-                                className="radioGroup-item"
-                                style={style.centerRadioButtons}
-                            />
-                        </RadioButtonGroup>
-                    </Grid>
-                    <Grid item xs={12} style={style.centerFields}>
-                        <RaisedButton label="Login" primary style={style.bottomButtons}
-                                      onClick={(event) => this.handleLoginClick(event)}/>
-                        <RaisedButton label="Register" secondary style={style}
-                                      onClick={(event) => this.handleLoginClick(event)}/>
-                    </Grid>
-                </Paper>
-            </Grid>
+                    </RadioButtonGroup>
+                </Grid>
+                <Grid item xs={12} className={classes.centerFields}>
+                    <RaisedButton label="Login" fullWidth primary className={classes.bottomButtons}
+                                  onClick={(event) => this.handleLoginClick(event)}/>
+                    <RaisedButton label="Register" fullWidth secondary className={classes.bottomButtons}
+                                  onClick={(event) => this.handleLoginClick(event)}/>
+                </Grid>
+            </AuthCard>
         );
     }
 }
