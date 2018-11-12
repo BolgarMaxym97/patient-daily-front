068d212c84839830ef54e6a8105a87338689016b
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-11-12 15:09:57 +0200

    'router'

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+
+    render() {
+        return (
+            <h1>MAIN</h1>
+        );
+    }
+}
+
+export default Home;
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,31 +1,56 @@
 import React, {Component} from 'react';
+import {MuiThemeProvider, getMuiTheme} from 'material-ui/styles';
 import {AppBar} from 'material-ui';
-import LeftMenu from '../../components/LeftMenu/LeftMenu'
+import LeftMenu from '../../components/LeftMenu/LeftMenu';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faUserCircle, faSlidersH, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
+import './Layout.css';
+
+library.add(faUserCircle, faSlidersH, faSignOutAlt);
+
 
 class Layout extends Component {
     constructor(props) {
         super(props);
         this.state = {
             drawerIsOpen: false,
+            title: document.title,
         }
     }
 
+    componentDidMount() {
+        return this.setState({title: document.title});
+    };
+
     leftMenuToogle = (state) => {
-      return this.setState({drawerIsOpen: state});
+        return this.setState({drawerIsOpen: state});
     };
 
     render() {
+        const theme = getMuiTheme({
+            palette: {
+                primary1Color: '#673ab7',
+                primary2Color: '#482880',
+                primary3Color: '#8561c5',
+                accent1Color: '#00e676',
+                accent2Color: '#00a152',
+                accent3Color: '#33eb91',
+            },
+        });
+
         return (
-            <div style={{backgroundColor: '#eeeeee', height: '100%'}}>
-                <main>
-                    <AppBar
-                        title={this.props.title}
-                        onLeftIconButtonClick={(e) => this.setState({drawerIsOpen: true})}
-                    />
-                   <LeftMenu drawerIsOpen={this.state.drawerIsOpen} leftMenuToogle={this.leftMenuToogle}/>
-                    {this.props.children}
-                </main>
-            </div>
+            <MuiThemeProvider muiTheme={theme}>
+                <div style={{backgroundColor: '#eeeeee', height: '100%'}}>
+                    <main>
+                        <AppBar
+                            title={this.state.title}
+                            onLeftIconButtonClick={(e) => this.setState({drawerIsOpen: true})}
+                        />
+                        <LeftMenu drawerIsOpen={this.state.drawerIsOpen} leftMenuToogle={this.leftMenuToogle}/>
+                        {this.props.children}
+                    </main>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
