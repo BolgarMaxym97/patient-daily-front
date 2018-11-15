181b51dd78b2c35132d8a276f08d3208c55ae9b5
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-11-15 11:28:39 +0200

    validator types

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,5 +1,20 @@
-const Validator = {
-
+const TYPES = {
+    EMAIL: 'email',
+    URL: 'url',
+    BOOLEAN: 'boolean',
+    NUMBER: 'number',
+    OBJECT: 'object',
+    STRING: 'string',
+    EMPTY: 'empty',
+    ALPHANUMERIC: 'alphaNumeric',
+    TIMESTRING: 'timeString',
+    DATESTRING: 'dateString',
 };
 
-export default Validator;
+const Validator = {
+    validate: (type, value) => {
+        return is[type](value);
+    },
+};
+
+export default {Validator, TYPES};
