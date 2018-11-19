700b2d491040b751448fcccee4055a366fe415b0
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-11-19 21:17:59 +0200

    hospital home wip

--- src/components/Home/Hospital/Hospital.js
+++ src/components/Home/Hospital/Hospital.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
+import Storage from '../../../app-storage';
 
 class Hospital extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            patients: [],
+            hospitalInfo: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({hospitalInfo: _.get(Storage.user(), [])});
+        this.setState({patients: _.get(Storage.user(), 'Patients', [])});
+    };
 
     render() {
+        console.log(this.state.patients);
         return (
             <h1>Главная страница больницы</h1>
         );
